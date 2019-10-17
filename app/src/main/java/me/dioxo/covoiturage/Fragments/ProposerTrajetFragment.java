9aaae4d87e632bc04f5a3d4d871a40932d674df1
Dioxo <amolinares19@gmail.com>
2019-10-17 20:48:24 +0200

    Charger localisations et choisir date

--- app/src/main/java/me/dioxo/covoiturage/Fragments/ProposerTrajetFragment.java
+++ app/src/main/java/me/dioxo/covoiturage/Fragments/ProposerTrajetFragment.java
@@ -1,37 +1,62 @@
 package me.dioxo.covoiturage.Fragments;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TimePicker;
 
+import androidx.fragment.app.Fragment;
+
+import java.util.Calendar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import me.dioxo.covoiturage.R;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link ProposerTrajetFragment.OnFragmentInteractionListener} interface
+ * {@link OnFragmentInteractionListener} interface
  * to handle interaction events.
  * Use the {@link ProposerTrajetFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ProposerTrajetFragment extends Fragment {
+    public class ProposerTrajetFragment extends Fragment implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    @BindView(R.id.spinner_depart)
+    Spinner spinnerDepart;
+    @BindView(R.id.spinner_arrive)
+    Spinner spinnerArrive;
+    @BindView(R.id.date)
+    EditText date;
+    @BindView(R.id.places)
+    EditText places;
+    @BindView(R.id.prix)
+    EditText prix;
+    @BindView(R.id.add)
+    ImageButton add;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
+    private Calendar calendar;
     private OnFragmentInteractionListener mListener;
-
+    private int dayFinal, monthFinal, yearFinal, hourFinal, minuteFinal;
     public ProposerTrajetFragment() {
         // Required empty public constructor
     }
@@ -67,7 +92,29 @@ public class ProposerTrajetFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_proposer_trajet, container, false);
+        View view = inflater.inflate(R.layout.fragment_proposer_trajet, container, false);
+        ButterKnife.bind(this, view);
+
+        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(getContext(),
+                    R.array.students_locations, android.R.layout.simple_spinner_item);
+
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerDepart.setAdapter(arrayAdapter);
+        spinnerArrive.setAdapter(arrayAdapter);
+
+        //Eviter introduire d'input
+        date.setInputType(InputType.TYPE_NULL);
+
+
+        //chaque fois qu'on click, on va selectionner la date
+        date.setOnClickListener(view1 -> selectDate() );
+        date.setOnFocusChangeListener((view1, hasFocus) -> {
+            if(hasFocus) {
+                selectDate();
+            }
+        });
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -93,6 +140,86 @@ public class ProposerTrajetFragment extends Fragment {
         super.onDetach();
         mListener = null;
     }
+
+    public void selectDate() {
+        calendar = Calendar.getInstance();
+
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int month = calendar.get(Calendar.MONTH);
+        int year = calendar.get(Calendar.YEAR);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), this, year, month, day);
+        datePickerDialog.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        yearFinal = year;
+        monthFinal = month + 1;
+        dayFinal = day;
+
+        calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(), this, hour, minute, true);
+
+        timePickerDialog.show();
+    }
+
+
+    @OnClick(R.id.add)
+    public void onViewClicked() {
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        hourFinal = hourOfDay;
+        minuteFinal = minute;
+
+
+        //Formato AAAA-MM-DD HH:MM:SS
+        StringBuilder fecha = formatoFecha();
+
+        date.setText(fecha);
+    }
+
+    private StringBuilder formatoFecha() {
+        //Formato AAAA-MM-DD HH:MM:SS
+
+        StringBuilder fecha = new StringBuilder();
+
+        //first concat year
+        fecha.append(yearFinal).append("-");
+
+        if (monthFinal >= 10) {
+            fecha.append(monthFinal).append("-");
+        } else {
+            fecha.append(0).append(monthFinal).append("-");
+        }
+
+        if (dayFinal >= 10) {
+            fecha.append(dayFinal).append(" ");
+        } else {
+            fecha.append(0).append(dayFinal).append(" ");
+        }
+
+        if (hourFinal >= 10) {
+            fecha.append(hourFinal).append(":");
+        } else {
+            fecha.append(0).append(hourFinal).append(":");
+        }
+
+        if (minuteFinal >= 10) {
+            fecha.append(minuteFinal);
+        } else {
+            fecha.append(0).append(minuteFinal);
+        }
+
+        return fecha;
+    }
+
+
 
     /**
      * This interface must be implemented by activities that contain this
