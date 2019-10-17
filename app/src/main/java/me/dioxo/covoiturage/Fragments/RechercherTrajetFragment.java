62d8427613e308b919fafa10df856ecd441468e9
Dioxo <amolinares19@gmail.com>
2019-10-17 20:16:22 +0200

    locations et DatePicker ont été ajouté

--- app/src/main/java/me/dioxo/covoiturage/Fragments/RechercherTrajetFragment.java
+++ app/src/main/java/me/dioxo/covoiturage/Fragments/RechercherTrajetFragment.java
@@ -1,35 +1,63 @@
 package me.dioxo.covoiturage.Fragments;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.text.InputType;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import me.dioxo.covoiturage.R;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link RechercherTrajetFragment.OnFragmentInteractionListener} interface
+ * {@link OnFragmentInteractionListener} interface
  * to handle interaction events.
  * Use the {@link RechercherTrajetFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class RechercherTrajetFragment extends Fragment {
+public class RechercherTrajetFragment extends Fragment implements  DatePickerDialog.OnDateSetListener{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    @BindView(R.id.txtTitle)
+    TextView txtTitle;
+    @BindView(R.id.spinner_depart)
+    Spinner spinnerDepart;
+    @BindView(R.id.spinner_arrive)
+    Spinner spinnerArrive;
+    @BindView(R.id.date)
+    EditText date;
+    @BindView(R.id.prix)
+    EditText prix;
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
+    private Calendar calendar;
     private OnFragmentInteractionListener mListener;
 
     public RechercherTrajetFragment() {
@@ -67,7 +95,35 @@ public class RechercherTrajetFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rechercher_trajet, container, false);
+        View view = inflater.inflate(R.layout.fragment_rechercher_trajet, container, false);
+        ButterKnife.bind(this, view);
+
+        ArrayList<String> locations = new ArrayList<>(
+                Arrays.asList(getResources().getStringArray(R.array.students_locations)));
+
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getContext()
+                , android.R.layout.simple_spinner_item,
+                locations);
+
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        //Eviter introduire d'input
+        date.setInputType(InputType.TYPE_NULL);
+
+
+        //chaque fois qu'on click, on va selectionner la date
+        date.setOnClickListener(view1 -> selectDate() );
+        date.setOnFocusChangeListener((view1, hasFocus) -> {
+            if(hasFocus) {
+                selectDate();
+            }
+        });
+
+        spinnerArrive.setAdapter(arrayAdapter);
+        spinnerDepart.setAdapter(arrayAdapter);
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -93,6 +149,25 @@ public class RechercherTrajetFragment extends Fragment {
         super.onDetach();
         mListener = null;
     }
+
+
+
+    public void selectDate() {
+        calendar = Calendar.getInstance();
+
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int month = calendar.get(Calendar.MONTH);
+        int year = calendar.get(Calendar.YEAR);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), this, year, month, day);
+        datePickerDialog.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        date.setText(day + "/" + month + "/" + year);
+    }
+
 
     /**
      * This interface must be implemented by activities that contain this
